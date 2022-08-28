d8fac244d2dca616434a26361ecae899e484f1c5
Nana Yaw Owusu-Koranteng <nyowusukoranteng@gmail.com>
2022-08-28 14:32:10 +0000

    Initial Commit

---
+++ GameOfThrones/GameOfThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesApp.swift
+//  GameOfThrones
+//
+//  Created by Nana Yaw Owusu-Koranteng on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
